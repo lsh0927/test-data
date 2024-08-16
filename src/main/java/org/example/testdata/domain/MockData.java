738b4f947b42d1717cf00650708394f8cd0029d2
lsheon0927@naver.com <lsheon0927@naver.com>
2024-08-16 16:44:08 +0900

    #9- 가짜 데이터 도메인 설계
    자동차 이름과 같은 고유명서처럼
    가짜 데이터를 사전식으로 저장해서 관리해야 할 경우를 도메인으로 설계함

---
+++ src/main/java/org/example/testdata/domain/MockData.java
@@ -0,0 +1,16 @@
+package org.example.testdata.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MockData {
+    private String mockDataType;
+    private String mockDataValue;
+
+
+}
